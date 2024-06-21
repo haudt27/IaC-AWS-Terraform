c586f72d7a1a596c977e639bf433e63b0cc8d3b0
Hau Dang Trung <126368523+haudt27@users.noreply.github.com>
2024-06-21 09:50:47 +0700

    Update main.tf

--- my-project/network/main.tf
+++ my-project/network/main.tf
@@ -8,8 +8,7 @@ terraform {
   }
 }
 
-#tfsec:ignore:aws-ec2-require-vpc-flow-logs-for-all-vpcs
-#tfsec:ignore:aws-ec2-no-public-ip-subnet
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.0.0"
